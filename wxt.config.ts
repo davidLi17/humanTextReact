62007d439dcfb666f969a484f88395fc2db35458
lihaoge <lihaoge@bytedance.com>
2025-11-27 00:24:31 +0800

    feat: 新增tailwind

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,17 @@
+/*
+ * @Author: lihaoge lihaoge@bytedance.com
+ * @Date: 2025-11-26
+ * @Description:
+ */
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [tailwindcss() as any],
+  }),
   manifest: {
     name: "人话翻译器",
     version: "1.1.5",
